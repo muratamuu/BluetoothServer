45dc0f0f1e48e149d047c5f58add864bb759fc56
Takashi, Murata <murata.muu@gmail.com>
2021-06-29 14:35:14 +0900

    initial commit

---
+++ BluetoothServer.pro
@@ -0,0 +1,5 @@
+TEMPLATE = app
+TARGET = server
+INCLUDEPATH += .
+QT += bluetooth
+SOURCES += main.cpp
---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include <QCoreApplication>
+#include <QDebug>
+#include <QBluetoothSocket>
+#include <QBluetoothServer>
+
+class BluetoothServer : public QObject
+{
+public:
+  std::function<void(QBluetoothSocket*)> OnRecv;
+
+  BluetoothServer()
+  : OnRecv([](auto){})
+  , server_{QBluetoothServiceInfo::RfcommProtocol}
+  , serviceInfo_{} {
+    setupServer();
+  }
+
+  ~BluetoothServer() {
+    foreach (auto* client, clients_)
+      client->abort();
+    clients_.clear();
+    serviceInfo_.unregisterService();
+    server_.close();
+  }
+
+  void Listen(const QString& serviceUuid, const QString& serviceName) {
+    serviceInfo_ = server_.listen(QBluetoothUuid(serviceUuid), serviceName);
+  }
+
+private:
+  void setupServer() {
+    connect(&server_, &QBluetoothServer::newConnection, [this] {
+      forever {
+        auto* socket = server_.nextPendingConnection();
+        //if (!socket) break;
+        setupClient(socket);
+        clients_.append(socket);
+      }
+    });
+    connect(&server_, static_cast<void(QBluetoothServer::*)(QBluetoothServer::Error)>(&QBluetoothServer::error),
+            [](QBluetoothServer::Error err) {
+      qDebug() << err;
+    });
+  }
+
+  void setupClient(QBluetoothSocket* socket) {
+    connect(socket, &QBluetoothSocket::disconnected, [this, socket] { clients_.removeOne(socket); });
+    connect(socket, &QBluetoothSocket::readyRead, [this, socket] { OnRecv(socket); });
+    connect(socket, QOverload<QBluetoothSocket::SocketError>::of(&QBluetoothSocket::error),
+            [this, socket] (QBluetoothSocket::SocketError err) {
+      if (err == QBluetoothSocket::RemoteHostClosedError)
+        clients_.removeOne(socket);
+      else
+        qDebug() << socket->errorString();
+    });
+  }
+
+  QBluetoothServer server_;
+  QBluetoothServiceInfo serviceInfo_;
+  QList<QBluetoothSocket*> clients_;
+};
+
+int main(int argc, char** argv) {
+  QCoreApplication app(argc, argv);
+
+  BluetoothServer server;
+  server.OnRecv = [](auto* socket) {
+    qDebug() << socket->readAll();
+  };
+  server.Listen("00001101-0000-1000-8000-00805F9B34FD", "Bluetooth Server 2");
+
+  qDebug() << "Bluetooth Listen OK";
+
+  return app.exec();
+}
+
